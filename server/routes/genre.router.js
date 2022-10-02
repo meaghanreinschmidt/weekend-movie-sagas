0ef88b67660730d14029799e876ae550b2a32743
Meaghan Reinschmidt <mreinschmidt@hotmail.com>
2022-10-01 20:29:50 -0500

    Add GET route for genres in router

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -3,8 +3,18 @@ const router = express.Router();
 const pool = require('../modules/pool')
 
 router.get('/', (req, res) => {
-  // Add query to get all genres
-  res.sendStatus(500)
+  const query = `SELECT "genres"."name" FROM "genres"
+                 JOIN "movies_genres" ON "movies_genres"."genre_id" = "genres"."id"
+                 JOIN "movies" ON "movies"."id" = "movies_genres"."movie_id"
+                 WHERE "movies"."id" = $1;`
+  pool.query(queryText, [req.params.id])
+    .then((result) => {
+      res.send(result.rows[0]);
+    })
+    .catch((err) => {
+      console.log('ERROR: Get genres', err);
+      res.sendStatus(500);
+    });
 });
 
 module.exports = router;
